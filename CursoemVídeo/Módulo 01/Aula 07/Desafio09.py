9b30996b72b1e910313b0640093ca042abcd124e
Marcelo Sampaio <marcelosampaiodev@outlook.com>
2023-09-12 22:54:38 -0300

    update

--- CursoemVídeo/Módulo 01/Aula 07/Desafio09.py
+++ CursoemVídeo/Módulo 01/Aula 07/Desafio09.py
@@ -1,2 +1,15 @@
 # Desafio 009
 # Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada.
+
+n = int(input('Digite um número: '))
+print(f'Tabuada do {n}:')
+print(f'{n} X 1 = {n*1}')
+print(f'{n} X 2 = {n*2}')
+print(f'{n} X 3 = {n*3}')
+print(f'{n} X 4 = {n*4}')
+print(f'{n} X 5 = {n*5}')
+print(f'{n} X 6 = {n*6}')
+print(f'{n} X 7 = {n*7}')
+print(f'{n} X 8 = {n*8}')
+print(f'{n} X 9 = {n*9}')
+print(f'{n} X 10 = {n*10}')
--- CursoemVídeo/Módulo 01/Aula 07/Desafio10.py
+++ CursoemVídeo/Módulo 01/Aula 07/Desafio10.py
@@ -1,2 +1,6 @@
 # Desafio 010
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,27.
+
+dinheiro = float(input('Digite quanto você tem na carteira para fazer a conversão em dólar: '))
+dolar = dinheiro * 3.27
+print(f'Saldo em reais R${dinheiro} - Saldo em dólar U${dolar}')
